92e49576fbaa6cce82fad567512dcfeca0a3075e
Selma Delgado <selmadelgado@gmail.com>
2018-04-13 20:11:04 -0500

    Building blocks for JS and CSS

---
+++ js/script.js
@@ -0,0 +1,46 @@
+var doCoolStuff = function (){
+  var currentClassName = document.getElementById('cool').className;
+  
+  if(currentClassName == 'cool'){
+    document.getElementById('cool').className = 'cool red';
+  } else {
+     document.getElementById('cool').className = 'cool';
+  }
+}
+
+
+var sayMyName = function (name){
+  alert('My name is: ' + name);
+} 
+
+var car = {
+          make: 'VW',
+          type: 'Polo',
+          color: 'blue',
+          isTurnedOn: false,
+          numberOfWeels: 4,
+          seats: [
+            'seat1', 
+            'seat2', 
+            'seat3', 
+            'seat4'
+          ],
+        
+          turnOn: function(){
+            this.isTurnedOn = true;
+          },
+        
+          fly: function(){
+            alert('fly');
+          },
+  
+          switchCar: function(isOn){
+            console.log('turn car ' + isOn);
+            if(isOn == true){
+              this.isTurnedOn = true;
+            } else {
+              this.isTurnedOn = false;
+            }
+          }
+      }
+    console.log('Hello there, friends');
